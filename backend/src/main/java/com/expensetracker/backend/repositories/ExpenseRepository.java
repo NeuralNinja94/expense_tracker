7248e7eb19070d9ded2d755d5dba80cb48eda54b
NeuralNinja94 <carmi_vaccaro@hotmail.com>
2025-12-17 15:34:23 +0100

    Verbessert

--- backend/src/main/java/com/expensetracker/backend/repositories/ExpenseRepository.java
+++ backend/src/main/java/com/expensetracker/backend/repositories/ExpenseRepository.java
@@ -2,7 +2,6 @@ package com.expensetracker.backend.repositories;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.expensetracker.backend.entities.Expense;
